56f59e8513db31633130c6ec9d8edbc0643999d2
Rohan859 <rohankundu859@gmail.com>
2024-02-22 00:41:50 +0530

    starting project

---
+++ src/main/java/com/shopping/databse/shopping/database/ShoppingDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.shopping.databse.shopping.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingDatabaseApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingDatabaseApplication.class, args);
+	}
+
+}
